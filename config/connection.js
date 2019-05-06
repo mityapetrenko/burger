e5d27bd0f588b8c0f0cb7bef1ae102812b36fb12
mityapetrenko <dimbaslv@gmail.com>
2019-05-06 14:34:17 -0500

    database updated

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 var mysql=require("mysql");
-var connection = mysql.createConnection({
+if (process.env.JAWSDB-URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+  
+}else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3307,
     user: "root",
@@ -7,6 +11,7 @@ var connection = mysql.createConnection({
     database: "burgers_db"
   });
   
+}
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
